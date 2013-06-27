546c7902145b907d39b8581e7fc44c02afe82c0d
Dinker Ambe <dinkerambe@gmail.com>
2013-06-27 15:02:37 -0700

    finished UserFriendDAO code

--- WEB-INF/src/UserFriendDAO.java
+++ WEB-INF/src/UserFriendDAO.java
@@ -8,7 +8,7 @@ public class UserFriendDAO{
 	final static String USER_TABLE= "tomcat_users";
 	final static String USER_FRIEND_TABLE = "user_friends"
 
-	public static initTable(){
+	public static void initTable(){
 		try{
 			SQLCMD.initConnection();
 			List<String> fields = new ArrayList<String>();
@@ -27,7 +27,7 @@ public class UserFriendDAO{
 	}
 
 
-	public static isFriend(UserFriendBean bean){
+	public static boolean isFriend(UserFriendBean bean){
 		boolean result;
 		try{
 			SQLCMD.initConnection();
@@ -60,7 +60,7 @@ public class UserFriendDAO{
 		return result;
 	}
 	
-	public static insertFriend(UserFriendBean bean){
+	public static void insertFriend(UserFriendBean bean){
 		try{
 			SQLCMD.initConnection();
 			long user1 = bean.getUserID();
@@ -90,10 +90,52 @@ public class UserFriendDAO{
 		}
 	}
 	
-	public static inserFriend_safe(UserFriendBean bean){
-		if(!isFriend(bean))
+	public static boolean insertFriend_safe(UserFriendBean bean){
+		boolean check = !isFriend(bean);
+		if(check)
 			insertFriend(bean);
+
+		return check;
 	}
+		
+	public static void removeFriend(UserFriendBean bean){
+		try{
+			SQLCMD.initConnection();
+			long user1 = bean.getUserID();
+			long user2 = bean.getFriendID();
+			String user1_str = bean.getUserID_str();
+			String user2_str = bean.getFriendID_str();
+			//MUST add unsigned long negative check property later
+
+			List<String> fields = new ArrayList<String>();
+			fields.add(UID1_DB);
+			fields.add(UID2_DB);
+
+			List<String values = new ArrayList<String>();
+			if(user1 < user2){
+				values.add(user1_str);
+				values.add(user2_str);
+			}
+			else{
+				values.add(user2_str);
+				values.add(user1_str);
+			}
+
+			SQLCMD.delete(USER_FRIEND_TABLE, fields, values);
+		}catch(Exception e){System.out.println("USER_FRIEND_DAO removeFriend failed");}
+		finally{
+			SQLCMD.closeConnection();
+		}
+	}
+
+	public static boolean removeFriend_safe(UserFriendBean bean){
+		boolean check = isFriend(bean);
+		if(check)
+			removeFriend(bean);
+
+		return check;
+	}
+
 			
 	
 
