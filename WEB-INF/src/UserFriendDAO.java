27b201658e969da944d723c0450192462da66161
Dinker Ambe <dinkerambe@gmail.com>
2013-06-27 14:45:58 -0700

    isFriend and insertFriend in UserFriendDAO

--- WEB-INF/src/UserFriendDAO.java
+++ WEB-INF/src/UserFriendDAO.java
@@ -1,6 +1,7 @@
 
 public class UserFriendDAO{
-
+	static ResultSet rs;
+	static Connection con;
 	final static String UID_DB = "user_id";
 	final static String UID1_DB = "user1_id";
 	final static String UID2_DB = "user2_id";
@@ -24,6 +25,77 @@ public class UserFriendDAO{
 			SQLCMD.closeConnection();
 		}
 	}
+
+
+	public static isFriend(UserFriendBean bean){
+		boolean result;
+		try{
+			SQLCMD.initConnection();
+			long user1 = bean.getUserID();
+			long user2 = bean.getFriendID();
+			String user1_str = bean.getUserID_str();
+			String user2_str = bean.getFriendID_str();
+			//MUST add unsigned long negative check property later
+
+			List<String> fields = new ArrayList<String>();
+			fields.add(UID1_DB);
+			fields.add(UID2_DB);
+
+			List<String values = new ArrayList<String>();
+			if(user1 < user2){
+				values.add(user1_str);
+				values.add(user2_str);
+			}
+			else{
+				values.add(user2_str);
+				values.add(user1_str);
+			}
+
+			rs = SQLCMD.select(USER_FRIEND_TABLE, fields, values);
+			result = rs.next();
+		}catch(Exception e){System.out.println("USER_FRIEND_DAO isFriend ERROR");}
+		finally{
+			SQLCMD.closeConnection();
+		}
+		return result;
+	}
+	
+	public static insertFriend(UserFriendBean bean){
+		try{
+			SQLCMD.initConnection();
+			long user1 = bean.getUserID();
+			long user2 = bean.getFriendID();
+			String user1_str = bean.getUserID_str();
+			String user2_str = bean.getFriendID_str();
+			//MUST add unsigned long negative check property later
+
+			List<String> fields = new ArrayList<String>();
+			fields.add(UID1_DB);
+			fields.add(UID2_DB);
+
+			List<String values = new ArrayList<String>();
+			if(user1 < user2){
+				values.add(user1_str);
+				values.add(user2_str);
+			}
+			else{
+				values.add(user2_str);
+				values.add(user1_str);
+			}
+
+			SQLCMD.insertField(USER_FRIEND_TABLE, fields, values);
+		}catch(Exception e){System.out.println("USER_FRIEND_DAO insert failed");}
+		finally{
+			SQLCMD.closeConnection();
+		}
+	}
+	
+	public static inserFriend_safe(UserFriendBean bean){
+		if(!isFriend(bean))
+			insertFriend(bean);
+	}
+			
+	
 
 
 }
