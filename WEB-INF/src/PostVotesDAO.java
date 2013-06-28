ea9df68e331175e417da8c20956b3c88cc28d1da
Dinker Ambe <dinkerambe@gmail.com>
2013-06-28 15:36:58 -0700

    PostVotersDAO competed

---
+++ WEB-INF/src/PostVotesDAO.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class PostVotesDAO{
+
+	final static String UID_DB = "user_id";
+	final static String POST_DB = "post_id";
+	
+	final static USER_TABLE = "tomcat_users";
+	final static String POST_TABLE = "posts"
+	final static String POST_VOTES_TABLE = "post_voters";
+	public static void initTable(){
+		try{
+			SQLCMD.initConnection();
+			List<String> fields = new ArrayList<String>();
+			fields.add(POST_DB + " BIGINT UNSIGNED NOT NULL");
+			fields.add(UID_DB + " BIGINT UNSIGNED NOT NULL");
+			fields.add("FOREIGN KEY (" + POST_DB + ") REFERENCES " + POST_TABLE + "(" + POST_DB + ")");
+			fields.add("FOREIGN KEY (" + UID_DB + ") REFERENCES " + USER_TABLE + "(" + UID_DB + ")");
+
+			SQLCMD.createTable(POST_VOTES_TABLE, fields);
+		}catch(Exception e){
+			System.out.println("POST_VOTERS_DAO: INIT TABLE FAILED " + e.getMessage());}
+		finally{
+			SQLCMD.closeConnection();
+		}
+	}
+
+	public static List<Long> grabPostVoters(long postID){
+		List<Long> result = new ArrayList<Long>();
+		try{
+			SQLCMD.initConnection();
+			rs = SQLCMD.select(POST_VOTES_TABLE, POST_DB, postID);
+			while(rs.next()){
+				result.add(new Long(rs.getLong(UID_DB));
+			}
+		}catch(Exception e){System.out.println("POST_VOTERS_DAO: grabPostVoters failed " + e.getMessage();}
+		finally{
+			SQLCMD.closeConnection();
+		}
+		return result;
+	}
+
+	public static List<long> grabVoterPosts(long userID){
+		List<Long> result = new ArrayList<Long>();
+		try{
+			SQLCMD.initConnection();
+			rs = SQLCMD.select(POST_VOTES_TABLE, UID_DB, userID);
+			while(rs.next()){
+				result.add(new Long(rs.getLong(POST_DB));
+			}
+		}catch(Exception e){System.out.println("POST_VOTERS_DAO: grabVoterPosts failed " + e.getMessage();}
+		finally{
+			SQLCMD.closeConnection();
+		}
+		return result;
+	}
+
+	public static int getVoteCount(long postID){
+		return grabPostVoters(postID).size();
+	}
+}
